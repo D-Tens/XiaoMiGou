a9544e8ca1f98d6fff6f2b99c7bd8ca5c4331e58
Caisson <61823825+D-Tens@users.noreply.github.com>
2020-05-24 21:21:39 +0800

    second commit

--- XiaoMiGou/XiaoMiGou-multiprocessing.py
+++ XiaoMiGou/XiaoMiGou-multiprocessing.py
@@ -6,7 +6,7 @@
 '''
 
 import random
-import time
+import time, datetime
 import requests
 import pymongo
 import multiprocessing
@@ -23,6 +23,14 @@ MONGO_CONNECTTON_NAME = 'xiaomigou'
 client = pymongo.MongoClient(MONGO_CONNECTTON_STRING)
 db = client[MONGO_DB_NAME]
 collention = db[MONGO_CONNECTTON_NAME]
+# 头信息
+headers = {
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"}
+# 无头浏览器
+option = ChromeOptions()
+# option.add_argument('--headless')  # 无界面浏览器的设置
+driver = webdriver.Chrome()
+driver.implicitly_wait(30)  # 隐性等待，最长等待30秒
 
 
 def pages_all(url):
@@ -31,8 +39,6 @@ def pages_all(url):
     :param url:
     :return:
     '''
-    headers = {
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"}
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
@@ -43,102 +49,73 @@ def pages_all(url):
         print('无法获取总页码')
 
 
-def chrome_option(url):
-    '''
-    设置Chrome浏览器
-    :param url:
-    :return:
-    '''
-    option = ChromeOptions()
-    option.add_argument('--headless')  # 无界面浏览器的设置
-    driver = webdriver.Chrome(options=option)
-    driver.implicitly_wait(30)  # 隐性等待，最长等待30秒
-    driver.get(url)
-    return driver
-
-
 def next_page(pages):
     '''
     设置爬取页数，获取商品列
     :param pages:
     :return:
     '''
-    for page in range(1, int(pages) + 1):
-        print('正在爬取第%s页' % (page))
-        cur_url = BASE_URL + "&page=" + str(page)
-        driver = chrome_option(cur_url)
-        if cur_url == driver.current_url:
-            url_sps = driver.find_elements_by_xpath('//li[contains(@class," g_over")]/a')
-            detail_list(url_sps)
-        driver.quit()
+    try:
+        for page in range(1, int(pages) + 1):
+            print('正在爬取第%s页' % (page))
+            cur_url = BASE_URL + "&page=" + str(page)
+            driver.get(cur_url)
+            if cur_url:
+                url_sps = driver.find_elements_by_xpath('//li[contains(@class," g_over")]/a')
+                yield [url_sp.get_attribute("href") for url_sp in url_sps]
+    finally:
+        driver.close()
         print('浏览器关闭')
 
 
-def detail_list(url_sps):
-    '''
-    获取每页全部详情链接
-    :param url_sps:
-    :return:
-    '''
-    ls = []
-    for url_sp in url_sps:
-        new_url = url_sp.get_attribute("href")
-        ls.append(new_url)
-    # print(ls)
-    mult_pool(ls)
-
-
-def detail_info(url_s):
+def detail_info(url):
     '''
     提取详情信息
     :param url_s:
     :return:
     '''
-    time.sleep(random.random() * 1)
-    driver2 = chrome_option(url_s)
-    # 标题
-    info = driver2.find_elements_by_xpath('//span[contains(@class,"title")]')  # 产品
-    title = info[0].text
-    # 推荐理由
-    summary = driver2.find_elements_by_xpath('//div[contains(@class,"goods-desc")]')  # 推荐理由
-    summary = summary[0].find_element_by_class_name("content").text + summary[0].find_element_by_class_name(
-        "rec-text").text
-    # 商品类别
-    catgory = driver2.find_elements_by_xpath('//div[contains(@class,"nav-wrap")]/div/a[3]')[0].text
-    # 券后价
-    now_price = driver2.find_elements_by_xpath('//div[contains(@class,"price")]/i')[0].text
-    # 原价
-    old_price = driver2.find_elements_by_xpath('//div[contains(@class,"price")]/b/i')[0].text
-    # 销量
-    nums = driver2.find_element_by_css_selector(".num i").text
-    # 优惠券
-    coupon = driver2.find_elements_by_xpath('//div[contains(@class,"coupon")]/span')[0].text
-    # 优惠券地址
-    sp_url = driver2.find_elements_by_xpath('//div[contains(@class,"buy-share")]/a')
-    sp_url = sp_url[0].get_attribute("href")
-    driver2.quit()
-    data = {
-        'title': title,
-        'summary': summary,
-        'catgory': catgory,
-        'now_price': now_price,
-        'old_price': old_price,
-        'nums': nums,
-        'coupon': coupon,
-        'sp_url': sp_url
-    }
-    save_data(data)
-
-
-def mult_pool(ls):
-    '''
-    设置多线程爬取
-    :param ls:
-    :return:
-    '''
-    pool = multiprocessing.Pool(3)
-    pool.map(detail_info, ls)
-    pool.close()
+    time.sleep(random.random() * 2)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        response.encoding = 'urf-8'
+        html = etree.HTML(response.text)
+        # 标题
+        title = html.xpath('//div[@class="detail-col"]/a/span[@class="title"]/text()')[0]
+        # 商品类别
+        catgory = html.xpath('//div[contains(@class,"nav-wrap")]/div/a[3]/text()')[0]
+        # 推荐理由
+        summary1 = html.xpath('//div[@class="goods-desc cf"]/div[@class="content"]/p//text()')
+        if summary1:
+            summary2 = ''.join(summary1)
+        summary3 = html.xpath('//div[@class="rec-text"]/span/text()')
+        summary = summary2 + summary3[0]
+        # 券后价
+        now_price = html.xpath('//div[contains(@class,"price")]/i/text()')[0]
+        # 原价
+        old_price = html.xpath('//div[contains(@class,"price")]/b/i/text()')[0]
+        # 销量
+        num = html.xpath('//span[@class="num"]/i/text()')[0]
+        nums_unit = html.xpath('//span[@class="num"]//text()')[2].strip()
+        nums = int(float(num) * 10000) if nums_unit == '万件' else num
+        # 优惠券
+        coupon_price = html.xpath('//div[contains(@class,"coupon")]/span/text()')[0]
+        # 优惠券地址
+        coupon_url = html.xpath('//div[contains(@class,"buy-share")]/a/@href')[0]
+        # 更新时间
+        update_time = datetime.datetime.now().strftime('%Y') + '-' + \
+                      html.xpath('//div[@class="time-num"]/span[@class="time"]/text()')[0].split('：')[1]
+        data = {
+            'title': title,
+            'summary': summary,
+            'catgory': catgory,
+            'now_price': now_price,
+            'old_price': old_price,
+            'nums': nums,
+            'coupon_price': coupon_price,
+            'coupon_url': coupon_url,
+            'update_time': update_time
+        }
+        save_data(data)
 
 
 def save_data(data):
@@ -156,5 +133,8 @@ def save_data(data):
 
 
 if __name__ == '__main__':
+    pool = multiprocessing.Pool(3)
     pages = pages_all(BASE_URL)
-    next_page(pages)
+    for ls in next_page(pages):
+        pool.map(detail_info, ls)
+    pool.close()
